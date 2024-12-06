4150993152b43dc021bf1ac8a67cdbf1e8b90ea5
alexrobincrabbe <157045573+alexrobincrabbe@users.noreply.github.com>
2024-12-06 17:34:42 +0100

    set up openAI API

--- lunacare_backend/api/urls.py
+++ lunacare_backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DemoApiView
+from .views import DemoApiView, OpenAIChatView
 
 urlpatterns = [
     path('api/', DemoApiView.as_view(), name='api-view'),
+    path('openai-chat/', OpenAIChatView.as_view(), name='openai-chat'),
 ]
